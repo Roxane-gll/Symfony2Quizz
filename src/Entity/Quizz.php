1e13cb671a5c6678b16389b0d7b7b7d15d8d52ce
Roxane-gll <guella.roxane@gmail.com>
2023-10-25 11:38:30 +0200

    Add Quizz from make:entity

--- src/Entity/Quizz.php
+++ src/Entity/Quizz.php
@@ -2,10 +2,57 @@
 
 namespace App\Entity;
 
+use App\Repository\QuizzRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuizzRepository::class)]
 class Quizz
 {
-    // ID unique
-    ///titre
-    // liste de questions []
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $questions = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getQuestions(): array
+    {
+        return $this->questions;
+    }
+
+    public function setQuestions(array $questions): static
+    {
+        $this->questions = $questions;
+
+        return $this;
+    }
 }
