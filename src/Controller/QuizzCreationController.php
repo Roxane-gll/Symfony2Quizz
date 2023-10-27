4aa1501b8838eb75c7e197e60d8b7d34f69d8054
Anais <anaismoreau@citia.org>
2023-10-27 14:01:43 +0200

    symfonyQuizz -- create quizz

--- src/Controller/QuizzCreationController.php
+++ src/Controller/QuizzCreationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Quizz;
 use App\Form\QuizzFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,19 +13,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class QuizzCreationController extends AbstractController
     {
         #[Route('/quizz/create', name: 'app_quizz_create')]
-        public function QuizzForm(Request $request) : Response
+        public function QuizzForm(Request $request, EntityManagerInterface $entityManager) : Response
         {
             $quizz = new Quizz();
             $form = $this->createForm(QuizzFormType::class, $quizz);
             $form->handleRequest($request);
 
             if ($form->isSubmitted() && $form->isValid()) {
-
-
+                $entityManager->persist($quizz);
+                $entityManager->flush();
+                return $this->redirectToRoute("app_quizz_participate");
             }
 
             return $this->render('quizz/quizzCreation.html.twig', [
-                'quizzForm' => $form
+                'quizzForm' => $form->createView(),
             ]);
         }
 
--- src/Form/QuizzFormType.php
+++ src/Form/QuizzFormType.php
@@ -3,8 +3,9 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class QuizzFormType extends AbstractType
@@ -12,10 +13,8 @@ class QuizzFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('questions', QuizzQuestionFormType::class)
-            ->add('save', SubmitType::class, ['label' => 'Create Quizz'])
+            ->add('title', TextType::class)
+            ->add('save', SubmitType::class)
         ;
     }
-
 }
